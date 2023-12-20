b08bef8e166fa67d8423ab36002528e42f3ca501
Andrew Fam <andrew.fam87@gmail.com>
2023-12-20 16:20:19 +0800

    Fix master

--- packages/native_crypto/lib/src/ciphers/aes/aes.dart
+++ packages/native_crypto/lib/src/ciphers/aes/aes.dart
@@ -241,12 +241,13 @@ class AES implements Cipher<AESCipherChunk> {
     }
 
     // TODO(hpcl): move these checks to the platform interface
+    /*
     if (bytes == null) {
       throw NativeCryptoException(
         code: NativeCryptoExceptionCode.nullError,
         message: 'Platform returned null bytes on chunk #$count',
       );
-    }
+    }*/
 
     if (bytes.isEmpty) {
       throw NativeCryptoException(
@@ -281,12 +282,13 @@ class AES implements Cipher<AESCipherChunk> {
     }
 
     // TODO(hpcl): move these checks to the platform interface
+    /*
     if (bytes == null) {
       throw NativeCryptoException(
         code: NativeCryptoExceptionCode.nullError,
         message: 'Platform returned null bytes on chunk #$count',
       );
-    }
+    }*/
 
     return bytes;
   }
